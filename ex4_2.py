60191015a9e1919eca98f0dc2496cc95c58131e2
thomas.schmitzer@oracle.com <thomas.schmitzer@oracle.com>
2024-11-19 17:28:22 -0600

    3

--- ex4_2.py
+++ ex4_2.py
@@ -1,11 +1,11 @@
-filename = "./labs/messier.txt"
+filename = r"./labs/messier.txt"
+no_name = "no name"
 for line in open(filename, encoding='latin_1'):
         if not line: break
         if line[0] == "M":
-            #print(line,end="")
             MessierNumber = line[:6].rstrip()
-            CommonName = "no name" if not line[6:40].lstrip().rstrip() else line[6:40].lstrip().rstrip()
+            CommonName = line[6:40].lstrip().rstrip()
             ObjectType = line[40:64].rstrip()
             Constellation = line[64:].rstrip()
-            print(f"|{MessierNumber}|{CommonName}|{ObjectType}|{Constellation}|")
+            print(f"|{MessierNumber}|{no_name if not CommonName else CommonName}|{ObjectType}|{Constellation}|")
             
