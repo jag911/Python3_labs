96583bf7cdac8ac64e19d0a524444d90847056fd
thomas.schmitzer@oracle.com <thomas.schmitzer@oracle.com>
2024-11-19 17:18:41 -0600

    index fix

--- ex4_2.py
+++ ex4_2.py
@@ -3,9 +3,9 @@ for line in open(filename, encoding='latin_1'):
         if not line: break
         if line[0] == "M":
             #print(line,end="")
-            MessierNumber = line[:5].rstrip()
-            CommonName = "no name" if not line[6:39].lstrip().rstrip() else line[6:39].lstrip().rstrip()
-            ObjectType = line[40:63].rstrip()
+            MessierNumber = line[:6].rstrip()
+            CommonName = "no name" if not line[6:40].lstrip().rstrip() else line[6:40].lstrip().rstrip()
+            ObjectType = line[40:64].rstrip()
             Constellation = line[64:].rstrip()
             print(f"|{MessierNumber}|{CommonName}|{ObjectType}|{Constellation}|")
             
