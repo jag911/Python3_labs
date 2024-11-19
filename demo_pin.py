908f3055eda8638ac853f1a7e625609d1df49d50
thomas.schmitzer@oracle.com <thomas.schmitzer@oracle.com>
2024-11-19 13:19:15 -0600

    asdf

---
+++ demo_pin.py
@@ -0,0 +1,15 @@
+
+
+master_pin = "0123"
+pin = None
+count = 0
+
+while pin != master_pin and count < 3:
+    pin = input("Enter PIN: ")
+    count += 1
+    if pin == master_pin:
+        print("Valid PIN")
+    else:
+        print("Invalid PIN")
+
+print("Done...")
