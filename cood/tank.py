85148c6571fae805457b1ace05f9a153a3b3e83e
jag911 <tschmitzer@gmail.com>
2024-11-21 16:44:02 -0600

    made importable modules

---
+++ cood/tank.py
@@ -0,0 +1,38 @@
+"""
+this module describes a tank class
+"""
+
+class Tank:
+
+    def __init__(self,country,model):
+        self.country = country
+        self.model = model
+        self._speed = 0
+        self._direction = 0
+        self._location = {'x':0,"y":0}
+        self._shells = 20
+        self._health = 100
+
+    def accel(self,increase):
+        self._speed += increase
+        return None
+
+    def deccel(self,decrease):
+        self._speed -= decrease
+        return None
+
+    def rotate_left(self,deg):
+        self._direction -= deg % 360
+        return None
+
+    def rotate_right(self,deg):
+        self._direction += deg %360
+        return None
+
+    def shoot(self):
+        self._shells -= 1
+        return None
+
+    def reload(self):
+        self._shells = 20
+        return None
--- ex7_1.py
+++ ex7_1.py
@@ -1,24 +1,30 @@
 import sys
 import re
+def main():
+    if sys.platform == 'win32' or sys.platform == 'win64':
+        pass # no one cares
+    else:
+        svc_file = r"/etc/services"
+    used_ports = {'tcp': set(), 'udp': set()}
+    pre_reg = re.compile(r"(\d{1,5})/(tcp|udp)")
+    with open(svc_file,"rt") as fh_in:
+        for line in fh_in:
+            line = line.strip()
+            if line[0] != "#" and not line.isspace():
+                m = pre_reg.search(line)
+                if m:
+                    num, proto = m.group(0).split('/')
+                    if int(num) < 1024:
+                        used_ports[proto].add(num)
+    print(1024-len(used_ports['udp']))
+    print(1024-len(used_ports['tcp']))
 
-if sys.platform == 'win32' or sys.platform == 'win64':
-    pass # no one cares
-else:
-    svc_file = r"/etc/services"
-used_ports = {'tcp': set(), 'udp': set()}
-pre_reg = re.compile(r"(\d{1,5})/(tcp|udp)")
-with open(svc_file,"rt") as fh_in:
-    for line in fh_in:
-        line = line.strip()
-        if line[0] != "#" and not line.isspace():
-            m = pre_reg.search(line)
-            if m:
-                num, proto = m.group(0).split('/')
-                if int(num) < 1024:
-                    used_ports[proto].add(num)
-print(1024-len(used_ports['udp']))
-print(1024-len(used_ports['tcp']))
+    print(set(range(1,1024)) - used_ports['tcp'])
+    print(set(range(1,1024)) - used_ports['udp'])
+    return None
 
-print(set(range(1,1024)) - used_ports['tcp'])
-print(set(range(1,1024)) - used_ports['udp'])
-
+if __name__ == "__main__":
+    import cProfile
+    cProfile.run("main()", "stats.prof")
+    import sys
+    sys.exit(0)
